dd71e7fd7de18ccebd8c6e0977cd62bf34a7e3b7
TheCodeDecode <akshaykumarr00894@gmail.com>
2020-10-22 13:39:04 +0530

    Updates

---
+++ precache-manifest.7bfcf799f199fde4a36d02fb85db0b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bc19c3fda3e2f523548f37509be10f",
+    "url": "/advice/index.html"
+  },
+  {
+    "revision": "1ecc6e57fcc0e3ee3601",
+    "url": "/advice/static/css/main.ec86d5c0.chunk.css"
+  },
+  {
+    "revision": "08422aefaadcc4ef39eb",
+    "url": "/advice/static/js/2.70efd924.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/advice/static/js/2.70efd924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecc6e57fcc0e3ee3601",
+    "url": "/advice/static/js/main.449d049b.chunk.js"
+  },
+  {
+    "revision": "6a97269788c817ff20b5",
+    "url": "/advice/static/js/runtime-main.053ab30c.js"
+  }
+]);
